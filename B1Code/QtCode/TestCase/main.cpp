6fae840bfa75d8ed5b1877856b0e608f9ba32aab
B1 <bwlow39@gmail.com>
2022-09-17 14:01:26 +1000

    ADD: main.cpp
    Call to test DataTranslator class.

--- B1Code/QtCode/TestCase/main.cpp
+++ B1Code/QtCode/TestCase/main.cpp
@@ -1,6 +1,18 @@
 //#include "mainwindow.h"
+#include "datatranslator.h"
+
 #include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QString data = "E:/Uni/00-Final Year Project/Argous/B1Code/TestCase.csv";
+    QString dest = data;
     //QApplication a(argc, argv);
     //MainWindow w;
     //w.show();
     //return a.exec();
+    DataTranslator test = DataTranslator();
+    test.SetFilePath(data, dest);
+    qDebug() << test.GenerateFile();
+    return 0;
+}
