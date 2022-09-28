0df0a0b9a08feeca3e7061f3e9691a32a135fcfc
Monique Kuhn <101131147@student.swin.edu.au>
2022-09-28 14:28:10 +1000

    ADD: Initial UI dark theme
    Dark theme that is unbranded

--- src/Win/Argous Qt code base/ArgouseBaseSoftware/main.cpp
+++ src/Win/Argous Qt code base/ArgouseBaseSoftware/main.cpp
@@ -1,9 +1,17 @@
 #include <QApplication>
+#include <QFile>
+
 #include "homepage.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    //Set the app stlye sheet
+    QFile styleSheetFile("Diffnes.qss");
+    styleSheetFile.open(QFile::ReadOnly);
+    QString styleSheet = QLatin1String(styleSheetFile.readAll());
+    a.setStyleSheet(styleSheet);
+
     a.setApplicationName("Argous Base Software");
 
     HomePage homePage;
