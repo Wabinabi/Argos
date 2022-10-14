ce7b67e1e0bcd179f84bca62b0cbcdd14018f324
B1 <bwlow39@gmail.com>
2022-10-14 20:29:48 +1100

    UPDATE: SDLogger.h documentation

--- src/esp32/as7-gamma/SdLogger.h
+++ src/esp32/as7-gamma/SdLogger.h
@@ -1,9 +1,6 @@
 /**
- * TODO: THIS BANNER
- * 
  * This is AS7's Logger. It prints to serial and SD card. 
  * The goal of this logger is to tie logging and comms into one location and delegate to a thread
- * Read this for getters and setters inside freertos https://forums.freertos.org/t/freertos-task-in-a-c-class/13984
  */
 #pragma once
 #ifndef AS7SDLOGGING_H
@@ -26,9 +23,12 @@
 #define LOG_LEVEL_VERBOSE   5
 #define CS_PIN 5
 
+/// @brief Logger update rate in Hertz
 #define LOGGER_FREQ 100      // Update rate in Hertz
 
 #define PLOTTER_ENABLE false // only prints plots, for testing.
+
+/// @brief Disables SD output for testing
 #define SD_DISABLED false
 
 namespace AS7 
@@ -36,27 +36,37 @@ namespace AS7
     class Logger
     {
     private:
+    public:
 
+        /// @brief Message Queue, acts as a buffer to hold messages before writing to SD
         std::queue<std::string> _msg_Queue;
+        /// @brief Logging Queue, acts as a buffer to hold log messages before writing to SD 
         std::queue<std::string> _log_Queue;
-        // Used for reading the message Queues for the scribe task
+        
+        /// @brief Deprecated
         SemaphoreHandle_t _sem_log;
         SemaphoreHandle_t _sem_msg;
         
-        // Mutex for the message Queues
+        /// @brief Mutex for Log Queue, ensures that the data is not overwritten and that the SD is only used by one thread at any one time
         SemaphoreHandle_t _sem_logQueueMutex;
+        /// @brief Mutex for Message Queue, ensures that the data is not overwritten and that the SD is only used by one thread at any one time
         SemaphoreHandle_t _sem_msgQueueMutex;
 
+        /// @brief Mutex for the data dictionary
         SemaphoreHandle_t _sem_dataMutex;
+        /// @brief Enqueue mutex to protect message queue
         SemaphoreHandle_t _sem_dataEnqMutex;
 
-        // Sem for Enabling/Disabling Task
+        /// @brief Enables/disables the task
         SemaphoreHandle_t _sem_enableMutex;
+        /// @brief Tracks if the thread is running or stopped. Written to by the thread.
         bool _running = false; // tracks if the thread is running or stopped
         bool _sdEnabled = false;
         bool _sdDetected = false;
 
+        /// @brief Data dictionary to store the current data that is actively being written to
         std::map<std::string, float> _activeData;     // Data that is actively written to
+        /// @brief Data dictionary to store the data before being written to SD
         std::map<std::string, float> _enqueuedData;   // Used as a buffer before being written
         bool _hasEnqueuedData = false;
 
@@ -68,17 +78,23 @@ namespace AS7
         File getDataFile();
         File getConfigFile();
 
+        /// @brief Opens the log file in Append mode
         void openLogFile();     // Opens Log file on SD in Append
+        /// @brief Opens the dat afile in Append mode
         void openDataFile();    // Opens Data file on SD in Append
         void closeLogFile();    // Closes Log File
         void closeDataFile();   // Closes Data File
 
-        const std::string _logFileLocation = "/as7.log"; // Location of the logging file, includes extension. Use .c_str() to for SD library
-        const std::string _dataFileLocation = "/data.csv"; // Location of the data file, includes extension. Use .c_str() to for SD library
+        /// @brief Location of the logging file, includes extension. Use .c_str() to for SD library
+        const std::string _logFileLocation = "/as7.log";
+        /// @brief Location of the logging file, includes extension. Use .c_str() to for SD library
+        const std::string _dataFileLocation = "/data.csv";
 
+        /// @brief Reference to the serial momnitor
         Print* _printer;
 
         Print* getPrinter();
+        /// @brief Level of messages to be printed to the Serial Monitor. All messages are always printed to the SD card
         int _verbosity = LOG_LEVEL_INFORM;
 
         std::string getTestMessage();
@@ -96,50 +112,123 @@ namespace AS7
         SemaphoreHandle_t getSemEnableMutex();
 
 
-        // Adds a message to be recorded to the SD card
-        // diagnotics probably? will flush out later
+        /**
+         * @brief Adds a message to be recorded to the SD card
+         * 
+         * @param message String to be logged
+         */
         void enqueueMsg(std::string message); 
-
-        // Adds a message to be sent to the console and onto the SD card
+        
+        /**
+         * @brief Adds a logging message to be logged to the SD card and sent to the serial monitor
+         * 
+         * The message may not be printed to the serial monitor if the verbosity level of the monitor is greater than the provided verbosity level.
+         * 
+         * This action enqueues the message to be written. Messages are buffered into the queue and then dequeued when the logger awakes every cycle.
+         * The logging rate is determined by `LOGGER_FREQ`
+         * 
+         * @param message The message to be sent
+         * @param verbosity The verbosity level. If unsure, use Logger.warn(), Logger.inform()...
+         */
         void enqueueLog(std::string message, int verbosity);
 
-        // for PLY generation, will need to be flushed out.
-        // String since we can also send header information
+        /// @brief For logging points to the PLY file. Deprecated
+        /// @param points The point to log in string format
         void enqueuePnt(std::string points);
 
+        /// @brief Dequeues a logging message from the log buffer
+        /// @return The next logging message as a `std::string`
         std::string dequeueLog();
 
-        // Implementation to start FreeRTOS tasks in classes
+        /// @brief Implementation to start FreeRTOS tasks in classes
         static void startTaskImpl(void*);
         TaskHandle_t th_logger;
         void mainTask(void* parameters);
 
+        /// @brief Initialises the SD card and clears previous files
         void initialiseSD();
 
-    public:
+    
         Logger(Print* output);
+        /**
+         * @brief Starts logger and intialises SD card.
+         * 
+         * @param core The core to run the logger on. Valid values are 0 and 1.
+         * @param priority The priority of the logger. The higher, the more likely it is to be scheduled in front of other tasks. Default 1.
+         * @param verbosity The verbosity level of the logger. If messages are less than the vebosity level, they are not written to the Serial Monitor.
+         */
         void start(int core=1, int priority=1, int verbosity=LOG_LEVEL_INFORM);
+        /// @brief Pauses the Logging thread. Logs can be enqueued but they are not acted on
         void pause();
+        /// @brief Resumes the logging thread
         void resume();
 
+        /// @brief  Returns the current health of the logger
+        /// @return True if logger is running with no issues
         bool running();
+        /// @brief Returns the current vebosity level of the logger
+        /// @return Verbosity Level (Int)
         int  verbosity();
 
+        /// @brief Sets the verbosity level of the logger
+        /// @param verbosity Int, use definitions `LOG_LEVEL_INFORM`, LOG_LEVEL_WARN`... defined in sdlogger.h
         void setVerbosity(int verbosity);
 
+        /// @brief Pushes a piece of data to be recorded as defined by the CSV mapping
+        /// @param key The data column
+        /// @param value The data value, as a float.
         void recordData(std::string key, float value);
+        /// @brief Pushes data to be enqueued and written. Typically done by the most imporatnt recorded data, the US Sensors.
         void pushData();
 
-        // The main logging tasks 
+        /**
+         * @brief Logs an inform-level message
+         * 
+         * Messages of this level should be useful to know but are not critical or concerning to the operation of the drone
+         * 
+         * @param message The message to be enqueued
+         */
         void inform(std::string message);
+        /**
+         * @brief Logs an warn-level message
+         * 
+         * Messages of this level should be concerning but otherwise do not have an immediate impact do the drone.
+         * An example warning may be a thread being paused programmatically.
+         * 
+         * @param message The message to be enqueued
+         */
         void warn(std::string message);
-        void error(std::string message);
+        /**
+         * @brief Logs an error-level message
+         * 
+         * Messages of this level has a direct and immediate impact on the drone, however, may still allow for operation of unaffected modules.
+         * An exmaple of an error would be a compass or accelerometer failure, though the drone will still collect data.
+         * 
+         * @param message The message to be enqueued
+         */
+        void error(std::string message);/**
+         * @brief Logs an fatal-level message
+         * 
+         * Messages of this level indicate that the drone is in a fault and cannot be operated.
+         * An example fatal error would be an emergency stop.
+         * 
+         * @param message The message to be enqueued
+         */
         void fatal(std::string message);
+        /**
+         * @brief Logs an verbose-level message
+         * 
+         * Messages of this level may not be informative but indicate some sort of running or function of the drone.
+         * An example verbose message may be the status logs showing the current state of the RX/TX Terminals
+         * 
+         * @param message The message to be enqueued
+         */
         void verbose(std::string message);
 
         void plot(std::string message);
 
-        void disableSDLogging();        // Disables SD Logging, even if SD Card is attached.
+        /// @brief Disables SD Logging, even if SD Card is attached.
+        void disableSDLogging();        
 
     };
 }
